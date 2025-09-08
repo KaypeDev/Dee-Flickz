080f6df7fdefe9ede8699ac4f8e67ad7cacb5adb
keanu porral <kaype.dev@gmail.com>
2025-09-08 15:45:08 -0700

    fix 404 not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,11 @@
 export default function NotFound() {
-    return(
-        <div>
-            <p>Not Found</p>
-        </div>
-    )
+  return (
+    <div className="flex flex-col items-center justify-center h-screen text-center px-4">
+      <h1 className="text-[45px] font-bold mb-2">404</h1>
+      <p className="text-[20px] mb-4">Page not found</p>
+      <a href="/" className="underline">
+        Home Page
+      </a>
+    </div>
+  );
 }
